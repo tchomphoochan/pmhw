46f6ec1330ad9b1a584bcc9b3625d49bd78b759d
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-10-29 12:24:56 -0400

    Warn when file exists instead of raising an error

--- make_memfile.py
+++ make_memfile.py
@@ -1,6 +1,7 @@
 """Generate memory file for rename table."""
 import math
 import sys
+import warnings
 
 params = {}
 
@@ -23,7 +24,12 @@ address_width = params["LogSizeMemory"]
 entry_width = counter_width + address_width
 entry_width_hex = math.ceil(entry_width / 4)
 
-with open(sys.argv[2], "xt") as outfile:
-    for _ in range(num_table_entries):
-        entry = "".join("0" for _ in range(entry_width_hex))
-        print(entry, file=outfile)
+try:
+    with open(sys.argv[2], "xt") as outfile:
+        for _ in range(num_table_entries):
+            entry = "".join("0" for _ in range(entry_width_hex))
+            print(entry, file=outfile)
+except FileExistsError:
+    warnings.warn(
+        f"memory file {sys.argv[2]} already exists", RuntimeWarning, stacklevel=2
+    )
