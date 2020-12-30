eb5d60ae30dcbeba156f16304b312eaf53d2d1e4
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-12-30 17:00:52 +0100

    Add some test code to C++ side

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <algorithm>
+#include <cstdint>
+#include <cstdio>
 #include <map>
+#include <numeric>
+#include <vector>
 
 #include "HostToPuppetmaster.h"
 #include "PuppetmasterToHostIndication.h"
-
-using namespace std;
 
 extern "C" {
 void connectal_setup();
@@ -17,15 +19,19 @@ void endTime(unsigned int points);
 class PuppetmasterToHostIndication
     : public PuppetmasterToHostIndicationWrapper {
 private:
-    map<int64_t, uint64_t> startTime;
-    map<uint64_t, uint64_t> endTime;
+    std::map<std::int64_t, std::uint64_t> startTime;
+    std::map<std::uint64_t, std::uint64_t> endTime;
 
 public:
-    void transactionStarted(uint64_t tid, uint64_t timestamp) {
+    void transactionStarted(std::uint64_t tid, std::uint64_t timestamp) {
+        printf("Started %lx at %lu\n", tid, timestamp);
+        fflush(stdout);
         startTime[tid] = timestamp;
     }
 
-    void transactionFinished(uint64_t tid, uint64_t timestamp) {
+    void transactionFinished(std::uint64_t tid, std::uint64_t timestamp) {
+        printf("Finished %lx at %lu\n", tid, timestamp);
+        fflush(stdout);
         endTime[tid] = timestamp;
     }
 
@@ -42,13 +48,29 @@ extern "C" void connectal_setup() {
     fflush(stdout);
 
     fpga = new HostToPuppetmasterProxy(IfcNames_HostToPuppetmasterS2H);
-    printf("Init the request to FPGA\n");
+    printf("Initialize the request interface to the FPGA\n");
     fflush(stdout);
 
     puppetmasterToHost = new PuppetmasterToHostIndication(
         IfcNames_PuppetmasterToHostIndicationH2S);
-    printf("Init the indication\n");
+    printf("Initialize the indication interface\n");
     fflush(stdout);
 }
 
-int main() { return 0; }
+int main() {
+    connectal_setup();
+
+    std::vector<unsigned> ids(64);
+    std::iota(ids.begin(), ids.end(), 0);
+
+    for (auto id : ids) {
+        fpga->enqueueTransaction(
+            id, (Object){1, 0, 123}, (Object){1, 0, 45}, (Object){1, 0, 12},
+            (Object){1, 0, 8}, (Object){1, 0, 9}, (Object){1, 0, 345},
+            (Object){1, 0, 643}, (Object){1, 0, 6445}, (Object){1, 1, 3},
+            (Object){1, 1, 34}, (Object){1, 1, 974}, (Object){1, 1, 76},
+            (Object){1, 1, 653}, (Object){1, 1, 445}, (Object){1, 1, 43},
+            (Object){1, 1, 4});
+    }
+    return 0;
+}
