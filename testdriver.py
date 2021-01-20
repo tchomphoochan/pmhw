68e5592e30ebe0a51db157feeaed21a6201fcea5
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2021-01-20 21:51:06 +0100

    Allow running multiple tests

--- testdriver.py
+++ testdriver.py
@@ -23,11 +23,19 @@ for test_file in test_dir.iterdir():
 
 # Read command line arguments.
 parser = ArgumentParser(description=__doc__)
-parser.add_argument(
-    "module",
-    metavar="InterfaceName",
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument(
+    "-m",
+    "--modules",
     choices=tests,
-    help="name of the interface that the module under testing implements",
+    nargs="+",
+    help="modules to test",
+)
+group.add_argument(
+    "-a",
+    "--all",
+    action="store_true",
+    help="test all modules",
 )
 parser.add_argument(
     "-d",
@@ -38,42 +46,50 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-# Run test.
-proc_dir = Path(args.directory)
-module_name = f"mk{args.module}Testbench"
-proc_path = proc_dir / module_name
-if not proc_path.is_file():
-    sys.exit(f"Module executable {proc_path} not found.")
-proc = subprocess.Popen(
-    [proc_path],
-    cwd=proc_dir,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    text=True,
-    bufsize=1,
-    preexec_fn=os.setsid,
-)
-assert proc.stdout is not None
-proc_output = proc.stdout
+# Run tests.
+for module in args.modules or tests:
+    print(f"Running tests for {module}... ", end="")
 
-# Check test output.
-expected = tests[args.module]
-results: List[str] = []
-passed = 0
-for i in range(len(expected)):
-    thread = Thread(target=lambda: results.append(proc_output.readline()))
-    thread.daemon = True
-    thread.start()
-    thread.join(TEST_TIMEOUT_SECONDS)
-    try:
-        assert not thread.is_alive(), "test timed out"
-        result = results[-1].rstrip()
-        assert result == expected[i], f"got\n\t{result}\nexpected\n\t{expected[i]}"
-    except AssertionError as e:
-        print(f"Test {i + 1} failed: {e}")
+    # Start process.
+    proc_dir = Path(args.directory)
+    module_name = f"mk{module}Testbench"
+    proc_path = proc_dir / module_name
+    if not proc_path.is_file():
+        sys.exit(f"Module executable {proc_path} not found.")
+    proc = subprocess.Popen(
+        [proc_path],
+        cwd=proc_dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        bufsize=1,
+        preexec_fn=os.setsid,  # necessary for the os.killpg call to work
+    )
+    assert proc.stdout is not None
+    proc_output = proc.stdout
+
+    # Check output.
+    expected = tests[module]
+    results: List[str] = []
+    passed = 0
+    for i in range(len(expected)):
+        thread = Thread(target=lambda: results.append(proc_output.readline()))
+        thread.daemon = True
+        thread.start()
+        thread.join(TEST_TIMEOUT_SECONDS)
+        try:
+            assert not thread.is_alive(), "test timed out"
+            result = results[-1].rstrip()
+            assert (
+                result == expected[i]
+            ), f"\n\tgot:     {result}\n\texpected: {expected[i]}"
+        except AssertionError as e:
+            print(f"\nTest {i + 1} failed: {e}", end="")
+        else:
+            passed += 1
+    if passed == len(expected):
+        print("all passed.")
     else:
-        passed += 1
-print("-" * 80)
-print(f"Passed {passed}/{len(expected)} tests.")
+        print(f"\n{passed}/{len(expected)} passed.")
 
-os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+    os.killpg(os.getpgid(proc.pid), signal.SIGTERM)  # kill subprocesses too
