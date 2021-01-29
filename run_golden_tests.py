2fe87e5c8325e9b290f4229f58c0f160ac751b68
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2021-01-29 17:39:18 +0100

    Wrap test runner code in functions

--- run_golden_tests.py
+++ run_golden_tests.py
@@ -4,55 +4,59 @@ import os
 import signal
 import subprocess
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from collections.abc import Mapping, Sequence
 from pathlib import Path
 from threading import Thread
 from typing import List
 
-# Read command line arguments.
-parser = ArgumentParser(description=__doc__)
-parser.add_argument(
-    "-k",
-    metavar="pattern".upper(),
-    dest="pattern",
-    help="only run tests which match the given substring",
-    default="",
-)
-parser.add_argument(
-    "-d",
-    metavar="directory".upper(),
-    dest="directory",
-    help="directory for golden tests",
-    default="../golden_tests",
-)
-parser.add_argument(
-    "-t",
-    metavar="timeout".upper(),
-    dest="timeout",
-    type=int,
-    help="test timeout in seconds",
-    default=5,
-)
-args = parser.parse_args()
 
-# Assemble tests.
-tests = {}
-test_dir = Path(args.directory).resolve()
-for test_file in test_dir.iterdir():
-    test_name = test_file.stem
-    if args.pattern not in test_name:
-        continue
-    with open(test_file, "rt") as f:
-        tests[test_name] = f.read().splitlines()
+def get_args() -> Namespace:
+    """Read command line arguments."""
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-k",
+        metavar="pattern".upper(),
+        dest="pattern",
+        help="only run tests which match the given substring",
+        default="",
+    )
+    parser.add_argument(
+        "-d",
+        metavar="directory".upper(),
+        dest="directory",
+        help="directory for golden tests",
+        default="../golden_tests",
+    )
+    parser.add_argument(
+        "-t",
+        metavar="timeout".upper(),
+        dest="timeout",
+        type=int,
+        help="test timeout in seconds",
+        default=5,
+    )
+    return parser.parse_args()
 
-# Run tests.
-for module, expected in tests.items():
-    print(f"Running golden test for {module}... ", end="")
 
+def collect_tests(test_dir: str, test_pattern: str) -> dict[str, list[str]]:
+    """Assemble golden tests."""
+    tests = {}
+    test_path = Path(test_dir).resolve()
+    for test_file in test_path.iterdir():
+        test_name = test_file.stem
+        if test_pattern not in test_name:
+            continue
+        with open(test_file, "rt") as f:
+            tests[test_name] = f.read().splitlines()
+    return tests
+
+
+def run_golden_test(executable: str, expected: Sequence[str], timeout: int) -> None:
+    """Spawn  process and compaere its output with the expected."""
     # Start process.
     proc_dir = Path(os.getcwd())
-    module_name = f"mk{module}Testbench"
-    proc_path = proc_dir / module_name
+    proc_path = proc_dir / executable
     if not proc_path.is_file():
         sys.exit(f"Module executable {proc_path} not found.")
     proc = subprocess.Popen(
@@ -75,7 +79,7 @@ for module, expected in tests.items():
         thread = Thread(target=lambda: results.append(proc_output.readline()))
         thread.daemon = True
         thread.start()
-        thread.join(args.timeout)
+        thread.join(timeout)
         if thread.is_alive():
             print("test timed out.", end="")
             break
@@ -92,3 +96,18 @@ for module, expected in tests.items():
         print(f"\n{passed}/{len(expected)} lines match.")
 
     os.killpg(os.getpgid(proc.pid), signal.SIGTERM)  # kill subprocesses too
+
+
+def main() -> None:
+    """Run golden tests as specified on the command line."""
+    args = get_args()
+
+    tests = collect_tests(args.directory, args.pattern)
+
+    for module, expected_output in tests.items():
+        print(f"Running golden test for {module}... ", end="")
+        run_golden_test(f"mk{module}Testbench", expected_output, args.timeout)
+
+
+if __name__ == "__main__":
+    main()
